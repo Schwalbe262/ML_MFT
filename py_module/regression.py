5766ad08188085ed99f8157902acea802b5f060a
Schwalbe262 <r1jae262@uos.ac.kr>
2022-01-10 21:17:34 +0900

    adding verify model

--- py_module/regression.py
+++ py_module/regression.py
@@ -1,14 +1,18 @@
 from pycaret.regression import *
 
-def regression_basic(dataset, parameter, algorithm="lgbm", frac_ratio=0.9, silent=True, save_en=False, save_model_name="model") :
+def regression_basic(dataset, parameter, algorithm="ligthgbm", frac_ratio=0.9, silent=True, save_en=False, save_model_name="model", new_feature_names=[]) :
 
 
     # split data for ML (train set / test set)
     data_seen = dataset.sample(frac=frac_ratio, random_state=765).reset_index(drop=True)
     data_unseen = dataset.drop(data_seen.index).reset_index(drop=True)
 
+    features = ["N1", "N2", "d1" , "d2", "freq", "move_tx", "move_rx", "offset_tx", "offset_rx", "per", "space1", "space2", "space3", "space4", "l1", "l2", "h1", "w1"] + new_feature_names
+
     # regresion setting
-    exp_reg101 = setup(data = data_seen, target = parameter, session_id=123, silent=silent, use_gpu=False, remove_perfect_collinearity=False) 
+    exp_reg101 = setup(data = data_seen, target = parameter, session_id=123, silent=silent, use_gpu=False, remove_perfect_collinearity=False, \
+    numeric_features = features, \
+    categorical_features = []) 
 
 
     # create model
---
+++ py_module/verify.py
@@ -0,0 +1,21 @@
+from pycaret.regression import *
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+
+
+def verify_model(model, data_seen, data_unseen, parameter) :
+
+    data_unseen_input = data_unseen.drop(parameter,axis=1)
+    data_unseen_result = model.predict(data_unseen_input)
+
+    number2 = len(data_unseen)
+    error_sum2 = sum(abs((data_unseen[parameter]-data_unseen_result)/data_unseen[parameter]))
+
+    R2_data_unseen = r2_score(data_unseen[parameter],data_unseen_result)
+    MAE_data_unseen = mean_absolute_error(data_unseen[parameter],data_unseen_result)
+    MSE_data_unseen = mean_squared_error(data_unseen[parameter],data_unseen_result)
+    RMSE_data_unseen = mean_squared_error(data_unseen[parameter],data_unseen_result, squared=False)
+    MPE_data_unseen = error_sum2/number2
+
+    return np.array([R2_data_unseen, MAE_data_unseen, MSE_data_unseen, RMSE_data_unseen, MPE_data_unseen])

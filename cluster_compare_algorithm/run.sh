ecd2cb6cec1f359e6884ba0970275220b234122e
Schwalbe262 <r1jae262@uos.ac.kr>
2022-01-20 14:00:22 +0900

    adding sh file (compare algorithm)

---
+++ cluster_compare_algorithm/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=47    # Cores per node
+#SBATCH --partition=cpu1        # Partition Name (cpu1, gpu1, hgx)
+##
+#SBATCH --job-name=SlurmExample
+#SBATCH -o SLURM.%N.%j.out         # STDOUT
+#SBATCH -e SLURM.%N.%j.err         # STDERR
+##
+
+hostname
+date
+
+## 
+#module add  ANACONDA/2020.11
+
+python ./cluster_compare_algorithm.py

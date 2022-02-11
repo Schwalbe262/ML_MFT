c9506471a1b972c9b563f18679d5c26548497dcb
Schwalbe262 <r1jae262@uos.ac.kr>
2022-02-11 14:33:46 +0900

    #015 클러스터코드 로그 지점 추가

--- C002_lightgbm_plot.py
+++ C002_lightgbm_plot.py
@@ -31,6 +31,8 @@ import os
 import argparse
 
 
+
+
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-rn', '--result_name', type=str, nargs='+', metavar='N', help='rseult_file name')
 parser.add_argument('-ln', '--log_name', type=str, nargs='+', metavar='N', help='log_file name')
@@ -41,7 +43,7 @@ args.rn = args.result_name[0]
 args.ln = args.log_name[0]
 
 f = open(args.ln, 'w')
-f.write(f'START')
+f.write(f'START : {datetime.now()}')
 f.close()
 
 # 2. data import
@@ -175,7 +177,7 @@ for n_estimators in [10,30,100,300,1000,3000,10000] :
         for num_leaves in [31,60,80,100,127] :
             for learning_rate in [0.001,0.01,0.05,0.1] :
                 f = open(args.ln, 'w')
-                f.write(f'n_est/max_d/num_leaves/learning_rate : {n_estimators}/{max_depth}/{num_leaves}/{learning_rate} : {datetime.datetime.now()}')
+                f.write(f'n_est/max_d/num_leaves/learning_rate : {n_estimators}/{max_depth}/{num_leaves}/{learning_rate} : {datetime.now()}')
                 f.close()
                 model = LGBMRegressor(random_state=765, n_estimators=n_estimators, max_depth=max_depth, num_leaves=num_leaves, learning_rate=learning_rate, **hyper_parameters)
                 model.fit(X_train,Y_train)
